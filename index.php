33f1ad36728c8e3514aa191f791db5b5bdbe0e23
TNT112000 <0972612200tuyen@gmail.com>
2021-09-27 15:00:58 +0700

    upload code

--- index.php
+++ index.php
@@ -8,15 +8,16 @@
   <title>restaurant</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css" class="">
   <link rel="stylesheet" href="base.css" class="">
   <link rel="stylesheet" href="grid.css" class="">
+  <link rel="stylesheet" href="style.css" class="">
+  <link rel="stylesheet" href="fonts/fontawesome-free-5.15.4-web/css/all.min.css" class="">
   <link rel="stylesheet" href="responsive.css" class="">
 </head>
 
 <body>
   <div class="main">
-    <div class="gird wide">
+    <div class="grid wide">
       <nav class="navbar navbar-expand-lg navbar-light bg-light edit-navbar edit-navbar">
         <div class="container-fluid container">
           <a class="navbar-brand" href="#">
@@ -45,20 +46,137 @@
         </div>
       </nav>
     </div>
-    <div id="carouselExampleSlidesOnly" class="carousel slide edit-slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="images/bg.jpg" class="d-block w-100 edit-block" alt="...">
-        </div>
-      </div>
+    <div id="" class="edit-slide" data-bs-ride="carousel">
+      <img src="images/bg.jpg" alt="" class="edit-block content-img-1 ">
+      <img src="images/bg-edit.jpg" alt="" class="edit-block content-img-2">
       <nav class="edit-search navbar navbar-light bg-light">
         <div class="container-fluid ">
           <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <input class="form-control me-2 edit-search-input" type="search" placeholder="Search for Food..."
+              aria-label="Search">
+            <button class="btn edit-btn btn-outline-success" type="submit">Search</button>
           </form>
         </div>
       </nav>
+    </div>
+    <div class="content">
+      <div class="explore-content">
+        <div class="gird wide">
+          <h1 class="content-title">
+            Explore Foods
+          </h1>
+          <div class="row ">
+            <div class="col l-4 m-6 c-12">
+              <div class="explore-tab">
+                <img src="images/pizza.jpg" alt="" class="explore-img">
+                <a href="" class="explore-name">Pizza</a>
+              </div>
+            </div>
+            <div class="col l-4 m-6 c-12">
+              <div class="explore-tab">
+                <img src="images/burger.jpg" alt="" class="explore-img">
+                <a href="" class="explore-name">Burger</a>
+              </div>
+            </div>
+            <div class="col l-4 m-6 c-12">
+              <div class="explore-tab">
+                <img src="images/momo.jpg" alt="" class="explore-img">
+                <a href="" class="explore-name">Momo</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="menu">
+        <div class="gird wide">
+          <h1 class="content-title">
+            Food Menu
+          </h1>
+          <div class="row ">
+            <div class="col l-6 m-6 c-12">
+              <div class="menu-tab">
+                <img src="images/menu-pizza.jpg" alt="" class="menu-img">
+                <div class="food-content">
+                  <div class="food-name">Food Title</div>
+                  <div class="food-rice">$2.3</div>
+                  <p class="food-text">Make width Italian Sauce , Chicken , anh orangeice vegetables.</p>
+                  <a href="" class="food-order">Order Now</a>
+                </div>
+              </div>
+            </div>
+            <div class="col l-6 m-6 c-12">
+              <div class="menu-tab">
+                <img src="images/menu-burger.jpg" alt="" class="menu-img">
+                <div class="food-content">
+                  <div class="food-name">Smoky Burger</div>
+                  <div class="food-rice">$2.3</div>
+                  <p class="food-text">Make width Italian Sauce , Chicken , anh orangeice vegetables.</p>
+                  <a href="" class="food-order">Order Now</a>
+                </div>
+              </div>
+            </div>
+            <div class="col l-6 m-6 c-12">
+              <div class="menu-tab">
+                <img src="images/menu-burger.jpg" alt="" class="menu-img">
+                <div class="food-content">
+                  <div class="food-name">Food Title</div>
+                  <div class="food-rice">$2.3</div>
+                  <p class="food-text">Make width Italian Sauce , Chicken , anh orangeice vegetables.</p>
+                  <a href="" class="food-order">Order Now</a>
+                </div>
+              </div>
+            </div>
+            <div class="col l-6 m-6 c-12">
+              <div class="menu-tab">
+                <img src="images/menu-pizza.jpg" alt="" class="menu-img">
+                <div class="food-content">
+                  <div class="food-name">Nice Burger</div>
+                  <div class="food-rice">$2.3</div>
+                  <p class="food-text">Make width Italian Sauce , Chicken , anh orangeice vegetables.</p>
+                  <a href="" class="food-order">Order Now</a>
+                </div>
+              </div>
+            </div>
+            <div class="col l-6 m-6 c-12">
+              <div class="menu-tab">
+                <img src="images/menu-pizza.jpg" alt="" class="menu-img">
+                <div class="food-content">
+                  <div class="food-name">Food Title</div>
+                  <div class="food-rice">$2.3</div>
+                  <p class="food-text">Make width Italian Sauce , Chicken , anh orangeice vegetables.</p>
+                  <a href="" class="food-order">Order Now</a>
+                </div>
+              </div>
+            </div>
+            <div class="col l-6 m-6 c-12">
+              <div class="menu-tab">
+                <img src="images/menu-pizza.jpg" alt="" class="menu-img">
+                <div class="food-content">
+                  <div class="food-name">Chicken Steam Momo</div>
+                  <div class="food-rice">$2.3</div>
+                  <p class="food-text">Make width Italian Sauce , Chicken , anh orangeice vegetables.</p>
+                  <a href="" class="food-order">Order Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="" class="menu-see-all menu-see">See all foods</a>
+        </div>
+      </div>   
+    </div>
+    <div class="footer">
+      <div class="grid wide">
+        <div class="footer-item">
+          <i class="footer-icon icon-fb fab fa-facebook"></i>
+          <i class="footer-icon icon-ig fab fa-instagram-square"></i>
+          <i class="footer-icon icon-tw fab fa-twitter"></i>
+        </div>
+        <div class="footer-item">
+          <p class="footer-text">
+            All rights reserved. Designed By <span class="footer-text-color">CSE.TLU</span>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
